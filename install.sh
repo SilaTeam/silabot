bd1cdf3c5456b9bd981d7ceb1f1e539c9ee4861c
Shahab <safaeishahab@yahoo.com>
2016-05-13 23:06:52 +0430

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+	echo "M E R B O T"
+	sudo apt-get update
+        sudo apt-get upgrade
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev
+
+fi
